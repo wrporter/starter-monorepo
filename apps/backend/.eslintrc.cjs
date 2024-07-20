43c873946a35f933678ff0e0254c060ff8cce4d3
wrporter <wesleyrporter@gmail.com>
2024-07-20 07:26:05 -0600

    Fix backend lint config to point to the right tsconfig

--- apps/backend/.eslintrc.cjs
+++ apps/backend/.eslintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
     extends: ['@repo/eslint-config/jest', '@repo/eslint-config'],
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+    },
     rules: {
         // Disable for Effect error _tag property.
         'no-underscore-dangle': 'off',
