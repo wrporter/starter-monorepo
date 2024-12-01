e417e4b54ac5635852b3316eae2ac3be98027b15
wrporter <wesleyrporter@gmail.com>
2024-12-01 07:57:36 -0700

    Improve CI performance
    - Attempt to use locally running Turbo cache from within Docker builds.

--- .github/docker/config.sh
+++ .github/docker/config.sh
@@ -30,7 +30,7 @@ function dockerBuild() {
     --label "build-info.git-repo=${GIT_REPO_URL}" \
     --label "build-info.git-user-email=${GIT_AUTHOR_EMAIL}" \
     --label "build-info.slack-channel=${SLACK_CHANNEL}" \
-    --build-arg TURBO_API="http://${DOCKER_HOST_IP:-"host.docker.internal"}:${TURBOGHA_PORT:-"41230"}" \
+    --build-arg TURBO_API="http://localhost:${TURBOGHA_PORT:-"41230"}" \
     --build-arg TURBO_TEAM=${TURBO_TEAM} \
     --build-arg TURBO_TOKEN=${TURBO_TOKEN} \
     $@
